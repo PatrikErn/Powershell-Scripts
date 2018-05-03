a3f423190b538d5d69ae463445991db670e5bf9c
Patrik Ernholdt <32933350+PatrikErn@users.noreply.github.com>
2018-05-03 16:32:22 +0200

    Add files via upload
    Get serial for your windows OS and list all software installed on device

---
+++ Simple scripts/Serial.ps1
@@ -0,0 +1,16 @@
+﻿#Written by Patrik Ernholdt
+#Simple script to display your Windows Serial Key and outputs it in a messagebox and then save it to a .txt
+# from the scripts run location.
+
+#Path
+$selfPath = (Get-Item -Path "." -Verbose).FullName
+$dllRelativePath = "........"
+$dllAbsolutePath = Join-Path $selfPath $dllRelativePath
+#Query and Output
+$serial = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey |Out-File  $selfPath\serial.txt 
+
+$Outfile = Get-Content $selfPath\serial.txt
+#Messagebox
+[System.Windows.MessageBox]::Show("Your Windows OS serial is $Outfile ")
+#Display .txt
+Invoke-Item $selfPath\serial.txt
---
+++ Simple scripts/Software.ps1
@@ -0,0 +1,12 @@
+﻿#Written by Patrik Ernholdt
+#Simple Script to list software installed on device
+#Displays a .txt with everything and saves it to location from where you run the script. 
+
+#Path
+$selfPath = (Get-Item -Path "." -Verbose).FullName
+$dllRelativePath = "........"
+$dllAbsolutePath = Join-Path $selfPath $dllRelativePath
+#Script and Output
+  Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize > $selfPath\Software.txt 
+#View result 
+Invoke-Item $selfPath\Software.txt 
