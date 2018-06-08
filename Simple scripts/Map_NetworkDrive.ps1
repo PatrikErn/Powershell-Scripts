228b54eea90c2ed8c57e15320fe5428832c281ee
Patrik Ernholdt <32933350+PatrikErn@users.noreply.github.com>
2018-06-08 08:58:16 +0200

    Update Map_NetworkDrive.ps1
    Sanitzed script

--- Simple scripts/Map_NetworkDrive.ps1
+++ Simple scripts/Map_NetworkDrive.ps1
@@ -1,6 +1,6 @@
 ﻿ $User = (get-wmiobject Win32_ComputerSystem).UserName.Split('\')[1]
- $G = "\\192.168.1.28\Gemensam" 
- $U = "\\192.168.1.28\$User"
+ $G = "\\Shared\Folder" 
+ $U = "\\User_Folder\$User"
 
 (New-Object -Com WScript.Network).MapNetworkDrive("G:" , "$G")
 (New-Object -Com WScript.Network).MapNetworkDrive("U:" , "$U") 
