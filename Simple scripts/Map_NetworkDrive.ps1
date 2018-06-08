bd241bffd69e3a3916c771df47fb1d7f33f388b8
Patrik Ernholdt <32933350+PatrikErn@users.noreply.github.com>
2018-06-08 08:57:05 +0200

    Add files via upload
    Simple script to map networkdrives , and also make map a users drive basee on logon name in format user logon name instead of domain/name. Useful if logon name is used for mapping drives.

---
+++ Simple scripts/Map_NetworkDrive.ps1
@@ -0,0 +1,7 @@
+﻿ $User = (get-wmiobject Win32_ComputerSystem).UserName.Split('\')[1]
+ $G = "\\192.168.1.28\Gemensam" 
+ $U = "\\192.168.1.28\$User"
+
+(New-Object -Com WScript.Network).MapNetworkDrive("G:" , "$G")
+(New-Object -Com WScript.Network).MapNetworkDrive("U:" , "$U") 
+
