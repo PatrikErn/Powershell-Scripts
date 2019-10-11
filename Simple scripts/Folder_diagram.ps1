e06c072ef8ef129f37865a183b437338f1c0c7ff
Patrik Ernholdt <32933350+PatrikErn@users.noreply.github.com>
2019-10-11 14:21:35 +0200

    Add files via upload
    Using psgraph module it will diagram folders and files with colors based on filetypes.

---
+++ Simple scripts/Folder_diagram.ps1
@@ -0,0 +1,39 @@
+﻿# Documentation http://psgraph.readthedocs.io
+# Module made by Kevin Marquette
+######################################
+#
+# Script made by
+# Patrik Ernholdt
+# 
+######################################
+# Based upon the example script made by Kevin Marquette
+# 
+# Requires Module PSGraph
+# https://github.com/KevinMarquette/PSGraph/tree/master/docs
+#
+# I wanted to experiment by giving certain files their own color to distinguish the filetypes.
+# To customize , just edit the values in $color.
+# This is a big help if you have a large diagram.
+
+
+
+# Color values. Add or edit colors and filetypes.
+$color = @{
+'.dll' = 'blue'
+'.xml' = 'green' 
+'.txt' = 'red'
+'.html' = 'cyan'
+'.xlsm' = 'magenta'
+}
+
+$files = Get-ChildItem C:\Temp -Recurse
+
+#Diagram
+graph folders  @{rankdir='LR'} {
+    #File nodes
+    node @{shape='rect'}
+    node $files -NodeScript {$_.fullname} @{label={$_.name};style='filled';fillcolor={$color[$_.extension]}} 
+    #Folder nodes
+    node $files.Where({$_.PSIsContainer}).FullName @{shape='folder';style='filled'; fillcolor='yellow'}
+    edge $files -FromScript {split-path $_.fullname} -ToScript {$_.fullname}
+} | Export-PSGraph -ShowGraph
